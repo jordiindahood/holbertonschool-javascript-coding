a2b842df64a6c301553e4acca56dbf8ba6342da8
jordiindahood <hazem.ouni123@gmail.com>
2024-05-17 10:10:56 +0100

    5-request_store.js

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 
-const fs = require('fs');
-const request = require('request');
+const fs = require("fs");
+const request = require("request");
 
 const url = process.argv[2];
 const Path = process.argv[3];
 
 request(url, (error, response, body) => {
   if (error) {
-    console.error('Error fetching the webpage:', error);
+    console.error("Error fetching the webpage:", error);
     return;
   }
-  fs.writeFile(Path, body, 'utf8', (writeErr) => {
+  fs.writeFile(Path, body, "utf8", (writeErr) => {
     if (writeErr) {
-      console.error('Error writing to file:', writeErr);
+      console.error("Error writing to file:", writeErr);
     }
   });
 });
