1eb8d6677a425aec29f91f3f981d387e3ded40da
jordiindahood <hazem.ouni123@gmail.com>
2024-05-31 23:41:33 +0100

    script 4

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,9 +1,9 @@
+/* eslint-disable jest/require-hook */
 const http = require('http');
 
 const app = http.createServer((req, res) => {
   res.writeHead(200);
   res.end('Hello Holberton School!');
 });
-// eslint-disable-next-line jest/require-hook
 app.listen(1245);
 module.exports = app;
